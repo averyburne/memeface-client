fe167638e302d3195650351e90df8fa167934323
Avery Burne <burneav@gmail.com>
2020-04-03 08:47:24 -0400

    Meme Create actually sends a file now

--- src/components/MemeCreate/MemeCreate.js
+++ src/components/MemeCreate/MemeCreate.js
@@ -1,61 +1,51 @@
 import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
-import axios from 'axios'
+import { post } from 'axios'
 import apiUrl from '../../apiConfig'
-import Layout from '../shared/Layout'
-import MemeForm from '../shared/MemeForm'
+// import Layout from '../shared/Layout'
+// import MemeForm from '../shared/MemeForm'
 
 const MemeCreate = props => {
-  // const [title, setTitle] = useState('')
   const [file, setFile] = useState([])
   const [createdMemeId, setCreatedMemeId] = useState(null)
-  const formData = new FormData()
 
-  const handleSubmit = (e) => {
+  const onFormSubmit = (e) => {
     e.preventDefault()
-    formData.append('file', file)
-    // formData.append('title', title)
     fileUpload(file)
       .then((response) => {
         setCreatedMemeId(response.data.meme._id)
       })
   }
 
-  const onChange = e => {
-    // const form = e.target.files[0]
-    // const data = new FormData(form)
-    console.log(e.target.files[0])
-    setFile(file => (e.target.files[0]))
-    // setTitle(title:e.target.files[0].name)
+  const onChange = function (e) {
+    setFile({ file: e.target.files[0] })
+    console.log(e.target.files[0].type)
   }
 
   if (createdMemeId) {
     return <Redirect to={`/memes/${createdMemeId}`} />
   }
 
-  const fileUpload = data => {
-    axios({
-      url: `${apiUrl} + /memes`,
-      method: 'POST',
-      data: formData,
-      contentType: false,
-      processData: false,
+  const fileUpload = file => {
+    const url = `${apiUrl}/memes`
+    const formData = new FormData()
+    formData.append('file', file)
+
+    const config = {
       headers: {
         Authorization: `Bearer ${props.user.token}`,
-        'Content-Type': 'multipart/form-data'
+        'content-type': 'multipart/form-data'
       }
-    })
+    }
+    return post(url, formData, config)
   }
 
   return (
-    <Layout>
-      <MemeForm
-        // meme={meme}
-        onChange={onChange}
-        handleSubmit={handleSubmit}
-        cancelPath="/"
-      />
-    </Layout>
+    <form onSubmit={onFormSubmit}>
+      <h1>File Upload</h1>
+      <input type="file" onChange={onChange} />
+      <button type="submit">Upload</button>
+    </form>
   )
 }
 
