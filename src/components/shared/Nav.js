45dec83b0eec8caf9895efdedc5a0a1f9cb62c50
Avery Burne <burneav@gmail.com>
2020-04-12 14:15:53 -0400

    removed useless Nav bar

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
+// import { Link } from 'react-router-dom'
+// <Link to='/'>Home</Link>
+// <Link to='/memes'>Memes</Link>
+// <Link to='/create-meme'>Create Meme</Link>
 const Nav = () => (
   <nav>
-    <Link to='/'>Home</Link>
-    <Link to='/memes'>Memes</Link>
-    <Link to='/create-meme'>Create Meme</Link>
+
   </nav>
 )
 
